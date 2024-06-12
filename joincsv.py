1d61aaaf912b3d2dd926f1d4585278cf2c03594b
laiacoronas <lclaiacoronas@gmail.com>
2024-06-12 20:25:03 +0200

    training

--- joincsv.py
+++ joincsv.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 
-data_dir = "\\Users\\lclai\\Desktop\\qm7\\qm7formulas"
+data_dir = "C:\\Users\\lclai\\Desktop\\qm7\\qm7e"
 
 csv_files = [os.path.join(data_dir, file) for file in os.listdir(data_dir) if file.endswith(".csv")]
 
@@ -13,4 +13,4 @@ for file in csv_files:
     df = pd.read_csv(file)
     all_data.append(df)
 combined_df = pd.concat(all_data, ignore_index=True)
-combined_df.to_csv("combined_data.csv", index=False)
+combined_df.to_csv("traingse.csv", index=False)
